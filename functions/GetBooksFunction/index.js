b6e8cebb7c7038cb95da597b9c524f84a4377179
Piso Mojado Grande <becky_weiss@hotmail.com>
2017-07-09 14:45:58 -0400

    Cache HTML content to S3 for 12hrs

--- functions/GetBooksFunction/index.js
+++ functions/GetBooksFunction/index.js
@@ -2,11 +2,76 @@ const crypto = require('crypto');
 const https = require('https');
 const querystring = require('querystring');
 const htmlparser = require('htmlparser');
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3();
+
+const S3_BUCKET = process.env.S3Bucket;
+const CACHE_PREFIX = process.env.CachePrefix;
+const LATEST_KEY = CACHE_PREFIX + "/latest.html";
+const MAX_CACHED_AGE_SECS = 60 * 60 * 12;
 
 function makeSessionId() {
     const buf10 = crypto.randomBytes(5);
     return buf10.toString('hex');
 }
+
+function fetchFromCache() {
+    return new Promise((resolve, reject) =>  {
+        const params = {
+            Bucket: S3_BUCKET,
+            Key: LATEST_KEY
+        };
+        console.log(`S3 request params ${JSON.stringify(params)}`);
+        s3.getObject(params, (err, data) => {
+            if (err) {
+                console.error(`S3 error ${JSON.stringify(err)}`);
+                if (err.code == 'NoSuchKey') {
+                    resolve(null);
+                } else reject(err);
+            } else {
+                const lastModified = Date.parse(data.LastModified);
+                const ageMsec = Date.now() - lastModified;
+                console.log(`Fetched ${data.ContentLength} bytes, age ${ageMsec / 1000} sec; last modified ${data.LastModified}`);
+                if (ageMsec > (1000 * MAX_CACHED_AGE_SECS)) {
+                    console.log('Content too old; fetching new');
+                    resolve(null);
+                } else {
+                    resolve(data.Body);
+                }
+            }
+        })
+    });
+}
+
+function putInCache(content) {
+    return new Promise((resolve, reject) => {
+        // write to both latest.html and a timestamped key
+        const timestampedKey = `${CACHE_PREFIX}/${(new Date()).toISOString()}-raw.html`;
+        console.log(`Writing ${content.length} to ${timestampedKey}, ${LATEST_KEY}`);
+        const promises = [LATEST_KEY, timestampedKey].map((s3key) => {
+            const params = {
+                Bucket: S3_BUCKET,
+                Key: s3key,
+                Body: content
+            };
+            return new Promise((innerResolve, innerReject) => {
+                s3.putObject(params, (err, data) => {
+                    if (err) reject(err);
+                    else resolve(data);
+                });
+            });
+        });
+        return Promise.all(promises);
+    }).then((allResults) => {
+        console.log('Caching successful');
+        return Promise.resolve(content);
+    }).catch((e) => {
+        console.warn(`Error caching content; continuing: ${e}`);
+        return Promise.resolve(content);
+    });
+}
+
 
 function fetchHtmlPromise(sessionId) {
     return new Promise((resolve, reject) => {
@@ -23,7 +88,7 @@ function fetchHtmlPromise(sessionId) {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Content-Length': Buffer.byteLength(postData)
             }
-        }
+        };
         
         console.log(`Making request ${JSON.stringify(httpsOptions)}`);
         var responseBody = "";
@@ -48,14 +113,24 @@ function fetchHtmlPromise(sessionId) {
     });
 }
 
+function fetchNew(sessionId) {
+    return fetchHtmlPromise(sessionId).then(putInCache);
+}
+
 exports.handler = (event, context, callback) => {
     console.log(JSON.stringify(event));
     
     const sessionId = makeSessionId();
     console.log(`Session ${sessionId}`);
     
-    fetchHtmlPromise(sessionId).then((htmlResponse) => {
-        callback(null, `Response size ${htmlResponse.length}`);
+    fetchFromCache().then((content) => {
+        if (content) {
+            return Promise.resolve(content);
+        } else {
+            return fetchNew(sessionId);
+        }
+    }).then((content) => {
+        callback(null, `Response size ${content.length}`);
     }).catch((e) => {
        callback(e); 
     });
