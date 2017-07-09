2a142a4256ef4523031d6cc816183289dbdedea4
Piso Mojado Grande <becky_weiss@hotmail.com>
2017-07-09 08:02:03 -0400

    Package htmlparser; jsdom doesn't appear loadable in lambda

--- functions/GetBooksFunction/index.js
+++ functions/GetBooksFunction/index.js
@@ -1,10 +1,51 @@
 const crypto = require('crypto');
 const https = require('https');
 const querystring = require('querystring');
+const htmlparser = require('htmlparser');
 
 function makeSessionId() {
     const buf10 = crypto.randomBytes(5);
     return buf10.toString('hex');
+}
+
+function fetchHtmlPromise(sessionId) {
+    return new Promise((resolve, reject) => {
+        const postData = querystring.stringify({
+            user_id: process.env.FCPLAccountId,
+            password: process.env.FCPLPassword
+        });
+        const httpsOptions = {
+            hostname: 'fcplcat.fairfaxcounty.gov',
+            port: 443,
+            path: '/uhtbin/cgisirsi/?ps=' + sessionId + '/0/0/57/30',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(postData)
+            }
+        }
+        
+        console.log(`Making request ${JSON.stringify(httpsOptions)}`);
+        var responseBody = "";
+        const req = https.request(httpsOptions, (res) => {
+            console.log(`STATUS: ${res.statusCode}`);
+            console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                responseBody += chunk;
+            });
+            res.on('end', () => {
+                console.log(`Response length: ${responseBody.length}`);
+                resolve(responseBody);
+            });
+        });
+        req.on('error', (e) => {
+            console.error(`Request failed: ${e.message}`);
+            reject(e);
+        });
+        req.write(postData);
+        req.end();        
+    });
 }
 
 exports.handler = (event, context, callback) => {
@@ -13,38 +54,11 @@ exports.handler = (event, context, callback) => {
     const sessionId = makeSessionId();
     console.log(`Session ${sessionId}`);
     
-    const postData = querystring.stringify({
-        user_id: process.env.FCPLAccountId,
-        password: process.env.FCPLPassword
+    fetchHtmlPromise(sessionId).then((htmlResponse) => {
+        callback(null, `Response size ${htmlResponse.length}`);
+    }).catch((e) => {
+       callback(e); 
     });
-    const httpsOptions = {
-        hostname: 'fcplcat.fairfaxcounty.gov',
-        port: 443,
-        path: '/uhtbin/cgisirsi/?ps=' + sessionId + '/0/0/57/30',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': Buffer.byteLength(postData)
-        }
-    }
-    
-    console.log(`Making request ${JSON.stringify(httpsOptions)}`);
-    var responseBody = "";
-    const req = https.request(httpsOptions, (res) => {
-        console.log(`STATUS: ${res.statusCode}`);
-        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-        res.setEncoding('utf8');
-        res.on('data', (chunk) => {
-            responseBody += chunk;
-        });
-        res.on('end', () => {
-            callback(null, responseBody);
-        });
-    });
-    req.on('error', (e) => {
-        console.error(`Request failed: ${e.message}`);
-        callback(e);
-    });
-    req.write(postData);
-    req.end();
+ 
 };
+
