0f9e46e032c3e7b0dfe6f1d685ec2a1a913809f3
Piso Mojado Grande <becky_weiss@hotmail.com>
2017-11-18 13:18:11 -0500

    Min height for the sidebars

--- www/app/layout.js
+++ www/app/layout.js
@@ -82,17 +82,21 @@ export const Layout = React.createClass({
     },
     render: function() {
         return (
-            <div className="pure-g">
-                <div className={["pure-u-1", styles.titlebar].join(' ')}>
-                    <h1>Your Books</h1>
-                    <UserAccountMenu username={this.state.username}/>
+            <div>
+                <div className="pure-g">
+                    <div className={["pure-u-1", styles.titlebar].join(' ')}>
+                        <h1>Your Books</h1>
+                        <UserAccountMenu username={this.state.username}/>
+                    </div>
                 </div>
-                <div className={["pure-u-1-8", styles.sidebar].join(' ')}>
-                </div>
-                <div className="pure-u-3-4">
-                    {this.props.children}
-                </div>
-                <div className={["pure-u-1-8", styles.sidebar].join(' ')}>
+                <div className="pure-g">
+                    <div className={["pure-u-1-8", styles.sidebar].join(' ')}>
+                    </div>
+                    <div className="pure-u-3-4">
+                        {this.props.children}
+                    </div>
+                    <div className={["pure-u-1-8", styles.sidebar].join(' ')}>
+                    </div>
                 </div>
             </div>
         )
